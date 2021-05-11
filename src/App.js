70e7175c7ec0b15ef75393ad3b71d21b4dce1eb2
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-05-10 23:35:04 -0300

    add formLogin component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { LoginForm } from './components/LoginForm'
 
 function App() {
   return (
+    <LoginForm />
+  )
+}
+
+export default App;
+
+
+
+
+/*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,7 +30,6 @@ function App() {
         </a>
       </header>
     </div>
-  );
-}
 
-export default App;
+
+*/
